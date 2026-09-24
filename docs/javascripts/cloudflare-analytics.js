9fcf82b761b1f277fc20d545873b05dbb9786b2f
dixi <116484779+dixiLOG@users.noreply.github.com>
2026-09-24 19:11:19 +0800

    chore: configure Cloudflare Web Analytics token

--- docs/javascripts/cloudflare-analytics.js
+++ docs/javascripts/cloudflare-analytics.js
@@ -1,7 +1,7 @@
 (() => {
   // Cloudflare Web Analytics site token.
   // Leave empty until the site has been created in Cloudflare Web Analytics.
-  const token = "";
+  const token = "76c7367dadc7456f9a4551bbd35fdce9";
 
   if (!token) return;
 
